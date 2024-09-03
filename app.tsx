44742cf787eb8d6691616f5724999411789269ad
André Ghisleni Raimann <andre@andreg.com.br>
2024-09-03 01:12:35 -0300

    Changes to be committed: 	new file:   app.tsx

---
+++ app.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text } from "react-native";
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Text>
+        Change code in the editor and watch it change on your phone! Save to get a shareable url.
+      </Text>
+    </SafeAreaView>
+  );
+}
